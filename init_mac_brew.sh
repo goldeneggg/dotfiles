deed056b197972e5c9cf3d6a81710fa3881ee1b2
goldeneggg <jpshadowapps@gmail.com>
2015-09-01 21:01:43 +0900

    ggrep for mac

--- init_mac_brew.sh
+++ init_mac_brew.sh
@@ -6,6 +6,8 @@
 
 #ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew update
+
 declare -ar APPS=("git" \
   "vim" \
   "wget" \
@@ -18,10 +20,18 @@ declare -ar APPS=("git" \
   "ctags" \
   "libevent" \
   "pstree")
-
 for app in ${APPS[@]}
 do
   brew install ${app}
 done
+
+# dupes
+brew tap homebrew/dupes
+declare -ar DUPES_APPS=("grep")
+for app in ${DUPES_APPS[@]}
+do
+  brew install homebrew/dupes/${app}
+done
+
 
 exit 0
