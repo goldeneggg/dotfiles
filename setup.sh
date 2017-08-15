d470f0f76c74334dde9e2c4ef8af269ebe77d042
goldeneggg <jpshadowapps@gmail.com>
2017-08-16 00:26:04 +0900

    bumpup keychain

--- setup.sh
+++ setup.sh
@@ -22,25 +22,25 @@ __EOT__
 }
 
 install() {
-    SETTINGS=$@
-    for setting in `echo ${SETTINGS}`
+  SETTINGS=$@
+  for setting in `echo ${SETTINGS}`
+  do
+    pushd ${setting}
+    for f in `ls -a`
     do
-        pushd ${setting}
-        for f in `ls -a`
-        do
-            if [ "${f}" != "." -a "${f}" != ".."  ]
-            then
-                if [ -f ${HOME}/${f} ]
-                then
-                    echo "already exist ${f} in HOME"
-                    rm ${HOME}/${f}
-                fi
-            fi
-        done
-        popd
-        s=`echo ${setting} | sed -e "s/\///g"`
-        stow -R --verbose=2 ${s}
+      if [ "${f}" != "." -a "${f}" != ".."  ]
+      then
+        if [ -f ${HOME}/${f} ]
+        then
+          echo "already exist ${f} in HOME"
+          rm ${HOME}/${f}
+        fi
+      fi
     done
+    popd
+    s=`echo ${setting} | sed -e "s/\///g"`
+    stow -R --verbose=2 ${s}
+  done
 }
 
 unset NAME VERSION GH_U GH_M
@@ -50,69 +50,69 @@ SIGN="${SIGN_LINUX}"
 
 while true
 do
-    case "$1" in
-        -h | --help ) usage; exit 1 ;;
-        -L | --linux ) SIGN=${SIGN_LINUX}; shift ;;
-        -M | --mac ) SIGN=${SIGN_MAC}; shift ;;
-        -n | --name ) NAME=$2; shift 2 ;;
-        -v | --version ) VERSION="-"$2; shift 2 ;;
-        --github-user ) GH_U=$2; shift 2 ;;
-        --github-mail ) GH_M=$2; shift 2 ;;
-        * ) break ;;
-    esac
+  case "$1" in
+    -h | --help ) usage; exit 1 ;;
+    -L | --linux ) SIGN=${SIGN_LINUX}; shift ;;
+    -M | --mac ) SIGN=${SIGN_MAC}; shift ;;
+    -n | --name ) NAME=$2; shift 2 ;;
+    -v | --version ) VERSION="-"$2; shift 2 ;;
+    --github-user ) GH_U=$2; shift 2 ;;
+    --github-mail ) GH_M=$2; shift 2 ;;
+    * ) break ;;
+  esac
 done
 
 if [ "${SIGN}" != "${SIGN_LINUX}" -a "${SIGN}" != "${SIGN_MAC}" ]
 then
-    echo "$0: SIGN is not assigned"
-    exit 1
+  echo "$0: SIGN is not assigned"
+  exit 1
 fi
 
 if [ ! ${GH_U} ]
 then
-    # if --github-user option is not specified, check GITHUB_USER env value
-    if [ ${GITHUB_USER} ]
-    then
-        GH_U=${GITHUB_USER}
-    else
-        echo "$0: github user or GITHUB_USER env value is not assigned"
-        exit 1
-    fi
+  # if --github-user option is not specified, check GITHUB_USER env value
+  if [ ${GITHUB_USER} ]
+  then
+    GH_U=${GITHUB_USER}
+  else
+    echo "$0: github user or GITHUB_USER env value is not assigned"
+    exit 1
+  fi
 fi
 
 if [ ! ${GH_M} ]
 then
-    # if --github-mail option is not specified, check GITHUB_MAIL env value
-    if [ ${GITHUB_MAIL} ]
-    then
-        GH_M=${GITHUB_MAIL}
-    else
-        echo "$0: github user or GITHUB_MAIL env value is not assigned"
-        exit 1
-    fi
+  # if --github-mail option is not specified, check GITHUB_MAIL env value
+  if [ ${GITHUB_MAIL} ]
+  then
+    GH_M=${GITHUB_MAIL}
+  else
+    echo "$0: github user or GITHUB_MAIL env value is not assigned"
+    exit 1
+  fi
 fi
 
 if [ `which stow` ]
 then
-    echo "stow is OK"
+  echo "stow is OK"
 else
-    echo "stow is not installed, please try again later stow install"
-    exit 1
+  echo "stow is not installed, please try again later stow install"
+  exit 1
 fi
 
 
 if [ "${NAME}" = "" ]
 then
-    if [ "${SIGN}" = "${SIGN_MAC}" ]
-    then
-      install `ls -1F | grep "/" | grep -v -- ${SIGN}`
-      install `ls -1F | grep "/" | grep -- ${SIGN}`
-    elif [ "${SIGN}" = "${SIGN_LINUX}" ]
-    then
-      install `ls -1F | grep "/" | grep -- ${SIGN}`
-    fi
+  if [ "${SIGN}" = "${SIGN_MAC}" ]
+  then
+    install `ls -1F | grep "/" | grep -v -- ${SIGN}`
+    install `ls -1F | grep "/" | grep -- ${SIGN}`
+  elif [ "${SIGN}" = "${SIGN_LINUX}" ]
+  then
+    install `ls -1F | grep "/" | grep -- ${SIGN}`
+  fi
 else
-    install ${NAME}${SIGN}${VERSION}
+  install ${NAME}${SIGN}${VERSION}
 fi
 
 # for neobundle.vim
@@ -169,7 +169,7 @@ fi
 # keychain
 if [ ! `which keychain` ]
 then
-  KEYCHAIN_VER="2.8.1"
+  KEYCHAIN_VER="2.8.3"
   KEYCHAIN_TAR=keychain-${KEYCHAIN_VER}.tar.bz2
   curl -L http://www.funtoo.org/distfiles/keychain/keychain-${KEYCHAIN_VER}.tar.bz2 -o ${KEYCHAIN_TAR}
   tar jxf ${KEYCHAIN_TAR}
